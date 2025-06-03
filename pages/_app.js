b9f849922de4fe160e4782dcf766731e594b4d55
Eugene <dryujin0715@mgmail.com>
2025-06-03 19:19:58 +0900

    구글 애널레틱스 try2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+  return (
+    <>
+      <GoogleAnalytics gaId={GA_ID} />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
